5c4d8073501958e5493b31fe04d5233715e116ec
Jane Doe <jane.doe@hwr-berlin.de>
2025-12-10 11:47:04 +0100

    WheelEats: Seiten ergänzt + Layout angepasst

--- app.py
+++ app.py
@@ -1,108 +1,91 @@
 from flask import Flask, render_template, redirect, url_for, request, abort, flash
 from flask_bootstrap import Bootstrap5
-import forms
 
 app = Flask(__name__)
 
 app.config.from_mapping(
-    SECRET_KEY = 'secret_key_just_for_dev_environment',
-    BOOTSTRAP_BOOTSWATCH_THEME = 'pulse'
+    SECRET_KEY='secret_key_just_for_dev_environment',
+    BOOTSTRAP_BOOTSWATCH_THEME='pulse'
 )
-
-from db import db, Todo, List, insert_sample  # (1.)
 
 bootstrap = Bootstrap5(app)
 
-@app.route('/')
-@app.route('/index')
+# WheelEats – Startseite
+@app.route("/")
+@app.route("/index")
 def index():
-    return redirect(url_for('todos'))
+    restaurants = [
+        {
+            "id": 1,
+            "name": 'Restaurant "Zur alten Laterne"',
+            "address": "Kantstraße 123, 10625 Berlin",
+            "features": ["Ebenerdig", "WC"],
+        },
+        {
+            "id": 2,
+            "name": "Pizzeria Bella Italia",
+            "address": "Friedrichstraße 45, 10117 Berlin",
+            "features": ["Ebenerdig", "WC", "Rampe", ">90cm Tür"],
+        },
+        {
+            "id": 3,
+            "name": "Curry 36",
+            "address": "Mehringdamm 36, 10961 Berlin",
+            "features": ["Ebenerdig"],
+        },
+    ]
+    return render_template("we_index.html", restaurants=restaurants)
 
-@app.route('/todos/', methods=['GET', 'POST'])
-def todos():
-    form = forms.CreateTodoForm()
-    if request.method == 'GET':
-        todos = db.session.execute(db.select(Todo).order_by(Todo.id)).scalars()  # !!
-        return render_template('todos.html', todos=todos, form=form)
-    else:  # request.method == 'POST'
-        if form.validate():
-            todo = Todo(description=form.description.data)  # !!
-            db.session.add(todo)  # !!
-            db.session.commit()  # !!
-            flash('Todo has been created.', 'success')
-        else:
-            flash('No todo creation: validation error.', 'warning')
-        return redirect(url_for('todos'))
 
-@app.route('/todos/<int:id>', methods=['GET', 'POST'])
-def todo(id):
-    todo = db.session.get(Todo, id)  # !!
-    form = forms.TodoForm(obj=todo)  # (2.)  # !!
-    if request.method == 'GET':
-        if todo:
-            if todo.lists: form.list_id.data = todo.lists[0].id  # (3.)  # !!
-            choices = db.session.execute(db.select(List).order_by(List.name)).scalars()  # !!
-            form.list_id.choices = [(0, 'List?')] + [(c.id, c.name) for c in choices]  # !!
-            return render_template('todo.html', form=form)
-        else:
-            abort(404)
-    else:  # request.method == 'POST'
-        if form.method.data == 'PATCH':
-            if form.validate():
-                form.populate_obj(todo)  # (4.)
-                todo.populate_lists([form.list_id.data])  # (5.)  # !!
-                db.session.add(todo)  # !!
-                db.session.commit()  # !!
-                flash('Todo has been updated.', 'success')
-            else:
-                flash('No todo update: validation error.', 'warning')
-            return redirect(url_for('todo', id=id))
-        elif form.method.data == 'DELETE':
-            db.session.delete(todo)  # !!
-            db.session.commit()  # !!
-            flash('Todo has been deleted.', 'success')
-            return redirect(url_for('todos'), 303)
-        else:
-            flash('Nothing happened.', 'info')
-            return redirect(url_for('todo', id=id))
+# WheelEats – Detailseite
+@app.route("/restaurants/<int:restaurant_id>")
+def restaurant_detail(restaurant_id):
+    return render_template("we_detail.html", restaurant_id=restaurant_id)
 
-@app.route('/lists/')
-def lists():
-    lists = db.session.execute(db.select(List).order_by(List.name)).scalars()  # (6.)  # !!
-    return render_template('lists.html', lists=lists)
 
-@app.route('/lists/<int:id>')
-def list(id):
-    list = db.session.get(List, id)  # !!
-    if list is not None:
-        return render_template('list.html', list=list)
-    else:
-        return redirect(url_for('lists'))
+# WheelEats – Formular „Restaurant hinzufügen“
+@app.route("/restaurants/new", methods=["GET", "POST"])
+def restaurant_new():
+    if request.method == "POST":
+        flash("Vielen Dank, dein Restaurant wurde eingereicht (Platzhalter).", "success")
+        return redirect(url_for("index"))
+    return render_template("we_new.html")
 
-@app.route('/insert/sample')
-def run_insert_sample():
-    insert_sample()
-    return 'Database flushed and populated with some sample data.'
 
+# WheelEats – Map-Seite
+@app.route("/map")
+def restaurant_map():
+    return render_template("we_map.html")
+
+
+# Fehlerseiten
 @app.errorhandler(404)
 def http_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
+
 
 @app.errorhandler(500)
 def http_internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template("500.html"), 500
 
-@app.get('/faq/<css>')
-@app.get('/faq/', defaults={'css': 'default'})
+
+# Beispielseiten
+@app.get("/faq/<css>")
+@app.get("/faq/", defaults={"css": "default"})
 def faq(css):
-    return render_template('faq.html', css=css)
+    return render_template("faq.html", css=css)
 
-@app.get('/ex/<int:id>')
-@app.get('/ex/', defaults={'id':1})
+
+@app.get("/ex/<int:id>")
+@app.get("/ex/", defaults={"id": 1})
 def ex(id):
     if id == 1:
-        return render_template('ex1.html')
+        return render_template("ex1.html")
     elif id == 2:
-        return render_template('ex2.html')
+        return render_template("ex2.html")
     else:
         abort(404)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
